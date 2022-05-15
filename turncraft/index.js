a927523c75eb6e367f9b13988c92f3a42be7528e
gdjolt8 <102922030+gdjolt8@users.noreply.github.com>
2022-05-15 08:25:47 -0400

    Update index.js

--- turncraft/index.js
+++ turncraft/index.js
@@ -11,7 +11,8 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = 'sd9j0asd208390147b32sd9a9d0ju!@#$%^&'
-mongoose.connect('mongodb://localhost:27017/login-app-db', {
+const e = 'mongodb+srv://trvlert:RrhE5a553UMc0LIC@turncraft.4bigr.mongodb.net/test'
+mongoose.connect(e, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -30,18 +31,6 @@ app.get("/user/:id", (req, res) => {
 
 });
 
-/* app.get("/catalog/:id/:name", (req, res) => {
-
-    var itemData = {
-        "item": req.params["id"],
-        "itemname": req.params["name"],
-        "price": Math.floor(Math.random() * 100),
-        "forsale": true
-    };
-
-    res.json(itemData);
-
-}); */
 
 app.get("/chess", (req, res) => {
 
@@ -58,13 +47,48 @@ app.get("/users", (req, res) => {
   })
 });
 
+app.post('/api/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+
+		const _id = user.id
+
+		const password = await bcrypt.hash(plainTextPassword, 10)
+
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: ';))' })
+	}
+})
+
+
+
 
 
 app.post('/api/register', async (req, res) => {
   console.log(req.body)
   
   const { username, password: plainTextPassword } = req.body
-  const length = User.length + 1;
   const coins = 0;
   
   if(!username || typeof username !== 'string') {
@@ -97,10 +121,14 @@ app.post('/api/register', async (req, res) => {
   }
 
   try {
+    const length = Database.length + 1;
     const response = await Database.create({
       username,
       coins,
-      date
+      date,
+      admin: false,
+      banned: false,
+      id: length
     })
    console.log('Database created!:' , response)
 } catch(error) {
@@ -138,16 +166,67 @@ app.post('/api/login', async(req, res) => {
     res.json({ status: 'ok', data: 'COMING SOON' })
 })
 
-app.post('/banuser', (req, res) => {
-  const user = users.find(user => user.name = req.body.banvictim)
-  if (user == null) {
-    return res.status(400).send("User does not exist")
+app.post('/api/banuser', async (req, res) => {
+  
+  const { user1, user2 } = req.body
+  const first = await Database.findOne({username: user1 }).lean().exec()
+  const second = await Database.findOne({username: user2 }).lean().exec()
+  /* user1 is who decided
+  user2 is who will get BANNED */
+
+  if(!first || !second) {
+    return res.json({ status: 'error', error: "One of those accounts don't exist"})
   }
+
+  if(first.admin == false) {
+    return res.json({ status: 'error', error:  first.admin + " " + second.admin + " " + ' Inflictor is NOT an admin!'})
+  }
+
+  if(second.banned == true) {
+    return res.json({ status: 'error', error: 'The user is already banned.'})
+  }
+
   try {
-    banned.push(banvictim);
-  } catch {
-    res.status(500).send("Oops, the server had an OOPSIE!");
+      const _id = user2.id
+      const banned = true
+
+      await Database.updateOne( 
+        {id: _id },
+         {banned: banned } 
+        )
+      return res.json({ status:'ok', data: second.username + ' was banned!'})
+  } catch (error) {
+     if(error.code === 11000) {
+       return res.json({ status: 'error', error: 'Something went wrong.'})
+     }
+     throw error
   }
+
+  
+
+
+
+})
+
+app.post('/api/moderator', async (req, res) => {
+  const { newmod } = req.body
+  const admin = true
+  try {
+       
+      const user = jwt.verify()
+
+      await Database.updateOne(
+        {user},
+        {
+          $set: { admin }
+        }
+      )
+
+
+  } catch (error) {
+
+  }
+  
 })
 
 
@@ -161,7 +240,14 @@ app.get("/messages", (req, res) => {
 
 app.post("/messages", async (req, res) => {
   try {
-      const message = { name: req.body.name, message: req.body.message, time: Date()}
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy
+
+      const message = { name: req.body.name, message: req.body.message, time: today}
       messages.push(message)
       res.status(201).send()
   } catch {
